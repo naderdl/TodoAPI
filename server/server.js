86d55aeb79d62685e8cc360299f85cb08503852b
Nader Daliri <naderdaliri@gmail.com>
2019-04-06 04:46:52 +0430

    get todos id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var { ObjectID } = require('mongodb');
 
 var { mongoose } = require("./db/mongoose");
 var { Todo } = require("./models/todo");
@@ -26,14 +27,23 @@ app.post("/todos", (req, res) => {
 
 app.get('/todos', (req, res) => {
   Todo.find().then(todos => {
-    res.send({todos})
+    res.send({ todos })
   }, e => {
     res.status(400).send(e);
   })
+})
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id
+  if (!ObjectID.isValid(id)) return res.status(404).send()
+  Todo.findById(id).then(todo => {
+    if (!todo) return res.status(404).send()
+    res.send({todo})
+  }).catch(e => res.status(400).send())
 })
 
 app.listen(3000, () => {
   console.log(`Here comes the magic: http://localhost:3000`);
 });
 
-module.exports = {app};
+module.exports = { app };
