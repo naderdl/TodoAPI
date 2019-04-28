444f50470acb11ff6f7a1586057d7b02c1abbe41
Nader Daliri <naderdaliri@gmail.com>
2019-04-28 21:47:24 +0430

    Added test for POST /users/login

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -204,7 +204,7 @@ describe('POST /users', () => {
           expect(user.email).toBe(email);
           expect(user.password).not.toBe(password);
           done();
-        })
+        }).catch(e => done(e))
       });
   })
 
@@ -228,3 +228,46 @@ describe('POST /users', () => {
       .end(done)
   })
 })
+
+describe('POST /users/login', done => {
+  it('should login user and return auth token', done => {
+    request(app)
+      .post('/users/login')
+      .send({
+        email: users[1].email,
+        password: users[1].password
+      })
+      .expect(200)
+      .expect(res => {
+        expect(res.header['x-auth']).toBeDefined();
+      })
+      .end((e, res) => {
+        if (e) return done(e);
+        User.findById(users[1]._id).then(user => {
+          expect(user.tokens[0]).toHaveProperty('access', 'auth');
+          expect(user.tokens[0]).toHaveProperty('token', res.headers['x-auth']);
+          done();
+        }).catch(e => done(e))
+      })
+  });
+
+  it('should reject invalid login', done => {
+    request(app)
+      .post('/users/login')
+      .send({
+        email: users[1].email,
+        password: 'asd123hkhl'
+      })
+      .expect(400)
+      .expect(res => {
+        expect(res.header['x-auth']).toBeUndefined();
+      })
+      .end((e, res) => {
+        if (e) return done(e);
+        User.findById(users[1]._id).then(user => {
+          expect(user.tokens).toHaveLength(0);
+          done();
+        }).catch(e => done(e))
+      })
+  });
+});
