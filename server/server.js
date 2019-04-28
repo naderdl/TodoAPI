045c2391ff7961aca38f14e17e637518d588200c
Nader Daliri <naderdaliri@gmail.com>
2019-04-28 22:21:33 +0430

    Added DELETE /users/me/token

--- server/server.js
+++ server/server.js
@@ -99,6 +99,14 @@ app.post('/users/login', (req, res) => {
   })
 })
 
+app.delete('/users/me/token', authenticate, (req, res) => {
+  req.user.removeToken(req.token).then(() => {
+    res.status(200).send();
+  }, () => {
+    res.status(400).send();
+  })
+})
+
 app.listen(port, () => {
   console.log(`Here comes the magic: http://localhost:${port}`);
 });
